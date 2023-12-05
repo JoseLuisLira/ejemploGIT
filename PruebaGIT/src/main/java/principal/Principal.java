6ea1f839a13b3471fadbbc8166eb7eece853bdc1
JoseLuisLira <151677315+JoseLuisLira@users.noreply.github.com>
2023-12-05 16:58:25 +0100

    Update dominguerol.java

--- PruebaGIT/src/main/java/principal/Principal.java
+++ PruebaGIT/src/main/java/principal/Principal.java
@@ -5,6 +5,9 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	System.out.println ("git hubl");}
+	System.out.println ("git hubl");
+	
+	
+	System.out.println ("editada por compañero");}
 
 }
